fba5e85ef37a1cc73f1b6b4c5f42326e309b9579
jyoung-man <jyp13.jyp@gmail.com>
2025-12-06 07:14:24 +0900

    Initial Commit

---
+++ applemusic_search/applemusic_searchApp.swift
@@ -0,0 +1,17 @@
+//
+//  applemusic_searchApp.swift
+//  applemusic_search
+//
+//  Created by 박재영 on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct applemusic_searchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ applemusic_searchTests/applemusic_searchTests.swift
@@ -0,0 +1,17 @@
+//
+//  applemusic_searchTests.swift
+//  applemusic_searchTests
+//
+//  Created by 박재영 on 12/6/25.
+//
+
+import Testing
+@testable import applemusic_search
+
+struct applemusic_searchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
